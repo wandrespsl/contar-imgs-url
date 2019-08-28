8ddd42d7209ecce4ff3517b4e43932a557867eb1
wandrespsl <weimarandrescastaneda@gmail.com>
2019-08-28 00:55:49 -0500

    server

--- scrape.js
+++ scrape.js
@@ -1,16 +1,31 @@
+const http = require('http');
+const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+const url = 'https://www.pslcorp.com/comprehensive-outsourcing-quality-assurance-support';
 
-request('https://www.pslcorp.com/', (error, response, html) => {
+request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
          const kwds = $('img');
         console.log("En este sitio hay: "+ kwds.length + " imagenes");   
-        // console.log(kwds.text());    
-        
-        // const output = kwds.find('img').length;
-        // const output = kwds.children('li').length;
-        // console.log(output);    
     }
+});
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.end(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(3002, () => {
+    console.log("El servidor está en escucha en el puerto 3002");
 });
