9d574af1836fb6ba62837b4a4bc9bed94af345d6
wandrespsl <weimarandrescastaneda@gmail.com>
2019-08-28 01:55:12 -0500

    Rendering HTML as Response

--- scrape.js
+++ scrape.js
@@ -1,31 +1,44 @@
-const http = require('http');
-const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
-const url = 'https://www.pslcorp.com/comprehensive-outsourcing-quality-assurance-support';
+const http = require("http");
+const fs = require("fs");
+const request = require("request");
+const cheerio = require("cheerio");
+const url =
+  "https://www.pslcorp.com/comprehensive-outsourcing-quality-assurance-support";
 
-request(url, (error, response, html) => {
+// request(url, (error, response, html) => {
+//     if (!error && response.statusCode == 200) {
+//         const $ = cheerio.load(html);
+
+//          const kwds = $('img');
+//         console.log("En este sitio hay: "+ kwds.length + " imagenes");
+//     }
+// });
+
+function contarImagenes() {
+  request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-
-         const kwds = $('img');
-        console.log("En este sitio hay: "+ kwds.length + " imagenes");   
+      const $ = cheerio.load(html);
+      const imgs = $("img");
+      const cantidadImagenes = imgs.length;
+      return console.log("En este sitio hay: " + cantidadImagenes + " imagenes");
     }
-});
+  });
+}
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.end(data);
-        }
-        response.end();
-    });
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write("File not found!");
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
 }
 
 http.createServer(onRequest).listen(3002, () => {
-    console.log("El servidor está en escucha en el puerto 3002");
+  console.log("El servidor está en escucha en el puerto 3002");
+  contarImagenes();
 });
