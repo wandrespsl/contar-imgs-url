f72380fb30b7e9c641946f2641df7c0efb704580
W Castaneda <wcastaneda@psl.com.co>
2019-08-30 10:30:36 -0500

    solo contador de imagenes

--- scrape.js
+++ scrape.js
@@ -1,44 +1,14 @@
-const http = require("http");
-const fs = require("fs");
 const request = require("request");
 const cheerio = require("cheerio");
 const url =
   "https://www.pslcorp.com/comprehensive-outsourcing-quality-assurance-support";
 
-// request(url, (error, response, html) => {
-//     if (!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-
-//          const kwds = $('img');
-//         console.log("En este sitio hay: "+ kwds.length + " imagenes");
-//     }
-// });
-
-function contarImagenes() {
-  request(url, (error, response, html) => {
+request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-      const imgs = $("img");
-      const cantidadImagenes = imgs.length;
-      return console.log("En este sitio hay: " + cantidadImagenes + " imagenes");
-    }
-  });
-}
+        const $ = cheerio.load(html);
 
-function onRequest(request, response) {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("index.html", null, function(error, data) {
-    if (error) {
-      response.writeHead(404);
-      response.write("File not found!");
-    } else {
-      response.write(data);
+         const imgs = $('img');
+        console.log("En este sitio hay: "+ imgs.length + " imagenes");
     }
-    response.end();
-  });
-}
-
-http.createServer(onRequest).listen(3002, () => {
-  console.log("El servidor está en escucha en el puerto 3002");
-  contarImagenes();
 });
+
